56e4bb2afd2f48e51bf3c6909a973b9a2a5c2687
Psyringe <psyringe@protonmail.com>
2015-12-07 00:24:43 +0330

    Added sha1_hex and sha1_base64 to Crypt module

--- src/Classes/crypt.rb
+++ src/Classes/crypt.rb
@@ -1,3 +1,11 @@
-class Crypt
+require 'digest/sha1'
 
+module Crypt
+  def sha1_hex(value)
+    return Digest::SHA1.hexdigest(value)
+  end
+
+  def sha1_base64(value)
+    return Digest::SHA1.base64digest(value)
+  end
 end
